3d781d1fc7398e8d15b671b85b8b427990f5817c
Milad_Mrk <milad.rastgoou@gmail.com>
2022-07-10 23:29:11 +0430

    [first] build intro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container bg-red-50 w-screen h-screen p-4 flex items-center  flex-col">
+      <div>
+        <img src='https://s6.uupload.ir/files/untitled-1_shg.png' alt='blood-logo'/>
+      </div>
+      <div dir='rtl'>
+        <h1 className='font-vazirB  text-gray-700'>تفسیر آزمایش خـون</h1>
+        <br/>
+        <span className='font-vazirL'>برنامه تفسیر آزمایش خون به شما کمک میکند بدون نیاز به دکتر ، ازمایش خود را تفسیر کنید</span>
+      </div>
+      
+      <div className='w-full h-full  flex items-center justify-center'>
+      <button className=' px-12 py-2  hover:ring-4 rounded-3xl bg-blue-500 font-vazirL text-blue-100'>ایجاد پروفایل</button>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: { 
+      fontFamily: {
+        'vazirL': ['VazirmatnL'],
+        'vazirB': ["VazirmatnB"],
+      },
+    },
+  },
+  plugins: [],
+}
